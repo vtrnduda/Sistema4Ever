6cbf194e2bf24a02cd3d715a02870e5cb205b5d4
vtrnduda <vtrnduda@gmail.com>
2023-12-05 00:45:13 -0300

    create Exceptions

--- src/regras_negocio/Fachada.java
+++ src/regras_negocio/Fachada.java
@@ -9,10 +9,18 @@ import repositorio.Repositorio;
 public class Fachada {
 	
 	private static Repositorio repositorio = new Repositorio();
-	private static int idEvento = 1;
+	private static int idEvento = 0;
 	private Fachada() {};
 
-	public static void criarEvento(String data, String descricao, int capacidade, double preco){
+	public static void criarEvento(String data, String descricao, int capacidade, double preco) throws Exception {
+
+		if (preco < 0){
+			throw new Exception("Não é possível criar um evento com preço negativo.");
+		}
+
+		if (data == null || data.isEmpty() || descricao == null || descricao.isEmpty()){
+			throw new Exception("Não é possível criar evento sem data ou descrição.");
+		}
 
 		while(true){
 			idEvento++;
