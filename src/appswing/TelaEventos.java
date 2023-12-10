d84f63b7b7dbda97a4b92006870fae02a9a12a1b
buenorafa <rafaelbuenooo@outlook.com>
2023-12-10 18:24:58 -0300

    TelaEventos

--- src/appswing/TelaEventos.java
+++ src/appswing/TelaEventos.java
@@ -1,15 +1,20 @@
 package appswing;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import java.awt.FlowLayout;
-import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+
+
 
 public class TelaEventos {
 
 	private JFrame frame;
-	private JLabel label;
+	private JTable eventoTable;
 
 	/**
 	 * Launch the application.
@@ -39,13 +44,45 @@ public class TelaEventos {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setEnabled(false);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		label = new JLabel("TELA DE EVENTOS");
-		label.setBounds(124, 104, 114, 32);
-		frame.getContentPane().add(label);
-	}
+		eventoTable = new JTable();
+		eventoTable.setBounds(6, 6, 284, 260);
+		
 
+		
+		
+		frame.getContentPane().add(eventoTable);
+		
+				
+		
+		JButton btnCriarEvento = new JButton("Criar evento");
+		btnCriarEvento.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCriarEvento.setBounds(302, 6, 142, 29);
+		frame.getContentPane().add(btnCriarEvento);
+		
+		JButton btnExcluirEvento = new JButton("Excluir evento");
+		btnExcluirEvento.setBounds(302, 47, 142, 29);
+		frame.getContentPane().add(btnExcluirEvento);
+		
+		JButton btnListarIngressos = new JButton("Listar ingressos");
+		btnListarIngressos.setBounds(302, 88, 142, 29);
+		frame.getContentPane().add(btnListarIngressos);
+		
+		JButton btnVoltar = new JButton("Voltar");
+		btnVoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnVoltar.setBackground(UIManager.getColor("Button.background"));
+		btnVoltar.setForeground(UIManager.getColor("Button.select"));
+		btnVoltar.setBounds(302, 237, 142, 29);
+		frame.getContentPane().add(btnVoltar);
+	}
 }
