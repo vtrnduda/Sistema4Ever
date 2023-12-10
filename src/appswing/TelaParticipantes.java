9e14abbcef198ebd9ffefd061ac6fcef7c613dee
Maria Eduarda Vitorino <maria.vitorino@picsel.com.br>
2023-12-09 21:48:43 -0300

    adding application windows

---
+++ src/appswing/TelaParticipantes.java
@@ -0,0 +1,43 @@
+package appswing;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class TelaParticipantes {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaParticipantes window = new TelaParticipantes();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TelaParticipantes() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
